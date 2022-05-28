0b287472025ef5bcb79998bc75da5b35304f478d
Eshitayasmin <eshita.emc2k17@gmail.com>
2022-05-28 11:03:09 +0600

    my profile add

--- src/Pages/DashBoard/MyProfile.js
+++ src/Pages/DashBoard/MyProfile.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const MyProfile = () => {
     return (
-        <div>
-            <h3>My Profile</h3>
+        <div className='flex justify-center my-16'>
+            <div>
+                <h1 className='text-2xl text-blue-400'>Name: Eshita Yasmin</h1>
+                <h2 className='text-xl text-red-400'>Email: eshita.emc2k17@gmail.com</h2>
+                <p className='text-md mb-4'>Education: I am studying in the final year of Zoology Department of Eden Women's College</p>
+                <p className='text-purple-300 text-lg'>Projects:</p>
+                <div>
+                    <ul>
+                        <li>1. Live Site Link:</li>
+                        <li>2. Live Site Link:</li>
+                        <li>3. Live Site Link:</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
